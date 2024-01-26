0794f359894cc1262872c42c09643d12f760d667
Danny Blessing <drblessing@gmail.com>
2024-01-25 19:00:06 -0800

    debugo

--- DataLemur/py_src/smallest_multiple.py
+++ DataLemur/py_src/smallest_multiple.py
@@ -1,49 +1,26 @@
-import math
-import time
+def gcd(a: int, b: int) -> int:
+    """Compute the greatest common divisor of a and b"""
+    while b > 0:
+        a, b = b, a % b
+    return a
 
 
-def smallest_multiple(target: int) -> int:
-    """Returns the smallest positive number that is evenly
-    divisible by all of the numbers from 1 to target."""
-
-    # Initialize the lowest common multiple (LCM) to 1
-    lowest_cm = 1
-
-    # The maximum lcm is the product of all numbers from 1 to target
-    max_lcm = math.prod(range(1, target + 1))
-
-    # Iterate through all numbers from 1 to max_lcm
-    for i in range(1, max_lcm + 1):
-        # Assume the number is the lcm
-        is_lcm = True
-        # Iterate through all numbers from 1 to target
-        for j in range(1, target + 1):
-            # If the number is not evenly divisible by any number
-            # from 1 to target, it is not the lcm
-            if i % j != 0:
-                is_lcm = False
-                break
-        # If the number is the lcm, return it
-        if is_lcm:
-            return i
-
-    return 1
+def lcm(a: int, b: int) -> int:
+    """Compute the lowest common multiple of a and b"""
+    return a * b // gcd(a, b)
 
 
-def smallest_multiple_fast(target: int) -> int:
-    return math.lcm(*range(1, target + 1))
+def smallest_multiple(n: int) -> int:
+    """Compute the lcm of all numbers from 1 to n."""
 
+    # Type check
+    if not isinstance(n, int):
+        raise TypeError("n must be an integer")
+    if n < 1:
+        raise ValueError("n must be positive")
 
-if __name__ == "__main__":
-    # Time the function
-    start = time.perf_counter()
-    smallest_multiple(20)
-    end = time.perf_counter()
-    slow_time = end - start
-    print(f"Slow time: {slow_time:0.6f} seconds")
-    start = time.perf_counter()
-    smallest_multiple_fast(20)
-    end = time.perf_counter()
-    fast_time = end - start
-    print(f"Fast time: {fast_time:0.6f} seconds")
-    print(f"Multiply factor: {slow_time / fast_time:0.2f}")
+    # Compute the lcm of all numbers from 1 to n
+    lcm_all = 1
+    for i in range(1, n + 1):
+        lcm_all = lcm(lcm_all, i)
+    return lcm_all
